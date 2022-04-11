2393c5595115a7137854b3df5e1c09501f1c7b03
Nabor Torres <arsentau@gmail.com>
2022-04-11 03:46:54 -0300

    Add Unit tests for Utilities

---
+++ app/trello/tests_utilities.py
@@ -0,0 +1,37 @@
+from unicodedata import category
+from unittest import result
+from django.test import TestCase
+from . import utilities
+from django.core.exceptions import ValidationError
+import re
+
+class Test_utilities(TestCase):
+    
+    def test_invalid_name(self):
+        name = "This is a very very long tittle that shall not pass"
+        with self.assertRaisesMessage(ValidationError, 'Too long: 51. Max. allowed: 20 characters'):
+            utilities.title_validator(name, 20)
+    
+    def test_valid_name(self):
+        result = None
+        name = "This is valid"
+        result = utilities.title_validator(name, 20)
+        self.assertEqual(result, None)
+            
+    def test_random_title_generator(self):
+        result = utilities.random_title_generator("bug", 5)
+        x = bool(re.search("^(bug-)[a-zA-Z]*-[0-9]*", result))
+        self.assertTrue(x)
+
+    def test_invalid_categoty(self):
+        category = "Invalid"
+        categories = [ "Mantainance", "Research", "Test"]
+        with self.assertRaisesMessage(ValidationError, 'The category: Invalid is not valid'):
+            utilities.categoty_validator(categories, category)
+
+    def test_valid_categoty(self):
+        result = None
+        category = "Research"
+        categories = [ "Mantainance", "Research", "Test"]
+        result = utilities.categoty_validator(categories, category)
+        self.assertEqual(result, None)
